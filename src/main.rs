7267928280adb6f2e1bdaa6fb2bc62ddc704a07f
Jon Binney <jon.binney@gmail.com>
2023-09-02 20:30:43 -0400

    Boilerplate for 2D camera and egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use bevy::{prelude::*};
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin)
+        .add_systems(Startup, setup)
+        .add_systems(Update, ui_example_system)
+        .run();
 }
+
+fn setup(mut commands: Commands, _asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn ui_example_system(mut contexts: EguiContexts) {
+    egui::Window::new("Debug Info").show(contexts.ctx_mut(), |ui| {
+        ui.label("TODO");
+    });
+}
+
